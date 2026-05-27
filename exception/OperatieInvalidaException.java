52578f85709cdc86ddf8133704e8764a6a57a210
aasdgkad <cfghxftdgugxugg@gmail.com>
2026-05-27 18:00:48 +0300

    Removed useless comment?

--- exception/OperatieInvalidaException.java
+++ exception/OperatieInvalidaException.java
@@ -1,6 +1,5 @@
 package com.pao.proiect.biblioteca.exception;
 
-// Pentru situațiile în care operația nu poate fi făcută (duplicat, invalid etc.)
 public class OperatieInvalidaException extends RuntimeException {
     public OperatieInvalidaException(String message) {
         super(message);
